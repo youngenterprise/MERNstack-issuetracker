cdf983c0e9f83ea4c2e3ba4adc0a66bc658d0391
Nakaw <nakawyoung@gmail.com>
2021-12-15 14:37:30 -0500

    Updated after a while

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,12 +1,18 @@
 const express=require('express');
-
 require('dotenv').config();
+const proxy=require('http-proxy-middleware');
 
 const app=express();
 
 app.use(express.static('public'));
 
-const UI_API_ENDPOINT = process.env. UI_API_ENDPOINT || 'http:localhost:3000/graphql';
+const apiProxyTarget=process.env.API_PROXY_TARGET;
+if(apiProxyTarget){
+    app.use('/graphql', proxy({target:apiProxyTarget}));
+}
+
+const UI_API_ENDPOINT = process.env. UI_API_ENDPOINT || '/graphql';
+API_PROXY_TARGET='http://localhost:3000'
 const env ={UI_API_ENDPOINT};
 
 app.get('/env.js', function(req,res){
