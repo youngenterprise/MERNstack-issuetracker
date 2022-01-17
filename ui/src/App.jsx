a635b38544e92780c13c97ab823200ed43fa0b73
Nakaw <nakawyoung@gmail.com>
2022-01-17 13:32:50 -0500

    Added simple routing for a SPA

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -4,9 +4,15 @@ import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import IssueList from './IssueList.jsx';
 
-const element = <IssueList />;
+import { HashRouter as Router } from 'react-router-dom';
+import Page from './Page.jsx';
+
+const element = (
+  <Router>
+    <Page />
+  </Router>
+);
 
 ReactDOM.render(element, document.getElementById('contents'));
 
