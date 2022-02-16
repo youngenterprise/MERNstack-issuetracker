70e42f045ea78d7586964a9dff8c17e8f37ed348
Nakaw <nakawyoung@gmail.com>
2022-02-16 15:15:36 -0500

    Added hydrate to make the pages interactive

--- ui/server/template.js
+++ ui/server/template.js
@@ -15,6 +15,10 @@ export default function template(body) {
     <body>
         <!-- Page generated from template. -->
         <div id="contents">${body}</div>
+
+        <script src="/env.js></script>
+        <script src="/vendor.bundle.js"></script>
+        <script src="/app.bundle.js"></script>
     </body>
     </html>
     `;
