d07e9221af2a55b627a875ae62d2802cf3bd1393
Nakaw <nakawyoung@gmail.com>
2022-02-25 13:07:16 -0500

    Fixed issue with rendering data in the UI

--- ui/server/render.jsx
+++ ui/server/render.jsx
@@ -4,21 +4,19 @@ import { StaticRouter } from 'react-router-dom';
 
 import Page from '../src/Page.jsx';
 import template from './template.js';
-
 import graphQLFetch from '../src/graphQLFetch.js';
 import store from '../src/store.js';
 
 async function render(req, res) {
   const initialData = await graphQLFetch('query{about}');
   store.initialData = initialData;
-
   const element = (
     <StaticRouter location={req.url} context={{}}>
       <Page />
     </StaticRouter>
   );
   const body = ReactDOMServer.renderToString(element);
-  res.send(template(body));
+  res.send(template(body, initialData));
 }
 
 export default render;
